86821297ecd75ec80509f37a575ebd16aaba2d31
Ross Middleton <rossmidd@gmail.com>
2020-07-29 20:24:26 +0100

    completed 2.13

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DetailCountry from './DetailCountry'
 
 const Countries = ({ countries, filter }) => {
+    const [showResults, setShowResults] = useState(false)
+    const onClick = (name) => setShowResults(name)
+
     const filtered = countries.filter(country => {
         return country.name.toLowerCase().includes(filter.toLowerCase())
     })
 
     const filterMultipleCountries = filtered.map((country, index) => {
-        return <div key={index}>{country.name}</div>
+        return (
+            <div key={index}>
+                {country.name}
+                <input type="submit" value="Show" onClick={()=> onClick(country.name)} />
+                {showResults === country.name ? <DetailCountry
+                    name={country.name}
+                    capital={country.capital}
+                    population={country.population}
+                    languages={country.languages}
+                    flag={country.flag}
+                /> : null}
+            </div>
+        )
     })
 
     if (filtered.length > 10) {
